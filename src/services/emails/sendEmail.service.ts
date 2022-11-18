9006175de6a35a9f7329c1ca089b62d4e2c451e9
Derick <d.a.s2000silva14@gmail.com>
2022-11-18 16:26:36 -0300

    feat: add documentation

--- src/services/emails/sendEmail.service.ts
+++
@@ -1,7 +0,0 @@
-import { IUserSendEmail } from "../../interfaces/user";
-import AppError from "../../errors/AppError";
-import "dotenv/config";
-
-const sendEmailService = async ({ email, subject, text }: IUserSendEmail) => {};
-
-export default sendEmailService;
