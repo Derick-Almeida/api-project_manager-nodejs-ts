fc76d30556c7f38b8be7562a4d2156c98f56d565
Derick <d.a.s2000silva14@gmail.com>
2022-11-18 17:39:15 -0300

    fix: config deploy

--- src/data-source.ts
+++ src/data-source.ts
@@ -11,18 +11,15 @@ const AppDataSource = new DataSource(
       }
     : {
         type: "postgres",
-        ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false,
-        url: process.env.DATABASE_URL,
+        host: process.env.DB_HOST,
+        port: 5432,
+        username: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB,
         logging: true,
         synchronize: false,
-        entities:
-          process.env.NODE_ENV === "production"
-            ? ["dist/src/entities/*.js"]
-            : ["src/entities/*.ts"],
-        migrations:
-          process.env.NODE_ENV === "production"
-            ? ["dist/src/migrations/*.js"]
-            : ["src/migrations/*.ts"],
+        entities: ["src/entities/*.ts"],
+        migrations: ["src/migrations/*.ts"],
       }
 );
 
