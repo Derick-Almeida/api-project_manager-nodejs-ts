71c9574ba08fa4c52401e354afb8d3b3f5932822
Derick <d.a.s2000silva14@gmail.com>
2022-11-19 19:38:37 -0300

    railway config

--- src/data-source.ts
+++ src/data-source.ts
@@ -12,10 +12,10 @@ const AppDataSource = new DataSource(
     : {
         type: "postgres",
         host: process.env.DB_HOST,
-        port: 5432,
+        port: Number(process.env.DB_PORT),
         username: process.env.DB_USER,
         password: process.env.DB_PASSWORD,
-        database: process.env.DB,
+        database: process.env.DB_NAME,
         logging: true,
         synchronize: false,
         entities: ["src/entities/*.ts"],
