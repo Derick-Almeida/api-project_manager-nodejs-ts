1b3e185d3ad6a6e5520d5d59f036a0e090237254
Derick <d.a.s2000silva14@gmail.com>
2022-11-20 14:24:26 -0300

    feat: add cors

--- src/app.ts
+++ src/app.ts
@@ -3,9 +3,12 @@ import express from "express";
 import "express-async-errors";
 import handleErrorMiddleware from "./middlewares/handleError.midleware";
 import appRoutes from "./routes";
+import cors from "cors";
 
 const app = express();
 app.use(express.json());
+
+app.use(cors());
 
 appRoutes(app);
 
