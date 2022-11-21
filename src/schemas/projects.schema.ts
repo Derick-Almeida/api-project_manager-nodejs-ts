7b33d8b119bead6b26cd4103444f4a83ea8fb72a
Derick <d.a.s2000silva14@gmail.com>
2022-11-20 22:25:27 -0300

    fix: project schema

--- src/schemas/projects.schema.ts
+++ src/schemas/projects.schema.ts
@@ -2,16 +2,13 @@ import * as yup from "yup";
 import { SchemaOf } from "yup";
 import { IProjectRequest } from "../interfaces/projects";
 
-const validUrl =
-  /^((ftp|http|https):\/\/)?(www.)?(?!.*(ftp|http|https|www.))[a-zA-Z0-9_-]+(\.[a-zA-Z]+)+((\/)[\w#]+)*(\/\w+\?[a-zA-Z0-9_]+=\w+(&[a-zA-Z0-9_]+=\w+)*)?$/gm;
-
 const createProjectSchema: SchemaOf<IProjectRequest> = yup.object().shape({
   name: yup.string().required().min(3),
   type: yup.string().required().min(5),
-  image: yup.string().required().matches(validUrl, "URL is not valid image"),
+  image: yup.string().required(),
   description: yup.string().required().min(10),
-  repository: yup.string().required().matches(validUrl, "URL is not valid repository"),
-  application: yup.string().required().matches(validUrl, "URL is not valid application"),
+  repository: yup.string().required(),
+  application: yup.string().required(),
 });
 
 export { createProjectSchema };
